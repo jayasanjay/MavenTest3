4f70f2389f2780e7cf18f35967fff7b813d6b733
SanJaiKru <sanjaikru@SanJaiKrus-MacBook-Air.local>
2017-09-08 23:24:41 +0530

    first test

---
+++ src/main/java/MavenTest3/MavenTest3/App.java
@@ -0,0 +1,13 @@
+package MavenTest3.MavenTest3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
